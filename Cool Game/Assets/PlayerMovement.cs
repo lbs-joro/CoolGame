75c732ff339f7273bfcc9735c5c709b97865d885
lbs-joro <johan.ronnas@ga.lbs.se>
2024-10-03 15:05:09 +0200

    La till kod i playermovment

--- Cool Game/Assets/PlayerMovement.cs
+++ Cool Game/Assets/PlayerMovement.cs
@@ -7,7 +7,8 @@ public class PlayerMovement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        print("Det här är spelarkoden!");
+        print("Hej!");
     }
 
     // Update is called once per frame
